37bb25455cafcf0fd13326974d8e63b729c89488
SandraSolberg <sandsol97@gmail.com>
2020-06-30 10:55:53 +0200

    Denne er nå så og si ferdig. Problem med å konvertere noen componenter til cross-site

--- Components/Screens/Profile/Profile.tsx
+++ Components/Screens/Profile/Profile.tsx
@@ -1,58 +1,67 @@
 import * as React from 'react';
 import {Alert, Button, Image, Linking, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import {Component, useEffect, useState} from "react";
+import {Header} from "react-native-elements";
 
 
 
 const serviceImage = [
-    {name:"Digital_Post", uri: "https://seeklogo.com/images/M/mail-icon-logo-28FE0635D0-seeklogo.com.png"  }
+    {
+        name:"Digital_Post", uri: "https://seeklogo.com/images/M/mail-icon-logo-28FE0635D0-seeklogo.com.png",
+        name2: "email-icon", uri2: "https://st2.depositphotos.com/2234823/8318/i/950/depositphotos_83181408-stock-photo-inbox-envelope-mail-icon-illustration.jpg"
+
+    }
 ];
 const userImages = [
     { name: "Profile", uri: "https://www.freeiconspng.com/uploads/profile-icon-28.png" }
 ];
-/*<Image source={{uri: serviceImage[0].uri}} style={{width: "100%", height: "100%"}} resizeMode={"cover" +
-                    ""}/>*/
+
+const persons =
+    {
+        name: "Linda Hansen",
+        birth: "01.01.2002",
+        address: "Dalen 24",
+        streetAddress: "Leikanger 6863",
+        email: "Linda.Hansen@gmail.com",
+        number: "12345678"
+
+
+    }
+
+
 export default function Profile() {
 
-    const[number, setNumber] = useState('mobilnummer');
-    const[address, setAddress] = useState('Adresse');
-    const[sted, setSted] = useState('Sted');
-    const[email, setEmail] = useState('E-post')
 
-
-    const numberPressButton =() => {
-        Alert.alert(" endra");
-    }
     return (
-        <View style={{ flex: 1, backgroundColor: "#87CEEB" }}>
-            <View style = {{padding: 5, alignItems: 'center', flexDirection: "row"}}>
-                <Text style = {{textTransform: "uppercase", fontSize: 13, fontWeight: "bold"}}>Person-og kontaktinformasjon</Text>
+        <View style={styles.container}>
+            <Header backgroundColor="white" centerComponent={{text: "Profil"}}/>
 
-                <TouchableOpacity style={styles.buttonContainer} onPress={() => Linking.openURL("https://www.digipost.no/")}>
+            <View style = {styles.upperScreen}>
+                <Text style = {{textTransform: "uppercase", fontSize: 12, fontWeight: "bold"}}>Person-og kontaktinformasjon</Text>
+                <TouchableOpacity style={styles.upperButtonContainer} onPress={() => Linking.openURL("https://www.digipost.no/")}>
                     <Image source={{uri: serviceImage[0].uri }} style={styles.images} />
-                    <Text style={{fontSize: 9,
+                    <Text style={{fontSize: 10,
                         fontWeight: "bold",
                         color: "gray" }}> DigiPost</Text>
                 </TouchableOpacity>
             </View>
-            <View style = {{padding: 5, alignItems: 'center'}}>
+            <View style = {{padding: 5, alignItems: 'center', justifyContent: "space-between"}}>
                 <Image source={{uri: userImages[0].uri}} style={styles.profileIcon}/>
                 <View style={styles.userInfoContainer}  >
-                    <Text style= {styles.textStyle}> Navn Etternavn </Text>
-                    <Text style={styles.textStyle}> 03.05.1992 </Text>
+                    <Text style= {styles.textStyle}> {persons.name} </Text>
+                    <Text style={styles.textStyle}> {persons.birth} </Text>
                 </View>
                 <View style={styles.userInfoContainer}>
-                    <Text style={styles.textStyle}>{address}</Text>
-                    <Text style={styles.textStyle}>{sted}</Text>
+                    <Text style={styles.textStyle}>{persons.address}</Text>
+                    <Text style={styles.textStyle}>{persons.streetAddress}</Text>
                 </View>
                 <View style={styles.userInfoContainer}>
-                    <Text style = {styles.textStyle}> {email}</Text>
-                    <Text style = {styles.textStyle}>{number} </Text>
+                    <Text style = {styles.textStyle}> {persons.email}</Text>
+                    <Text style = {styles.textStyle}>{persons.number} </Text>
                 </View>
-                <View style = {{padding: 10, alignItems: 'center', justifyContent: "space-between"}}>
-                    <Button title="Endre adresse" color="#66CDAA" onPress={() => Linking.openURL("https://www.skatteetaten.no/person/folkeregister/flytte/?utm_source=bing&utm_medium=cpc&utm_campaign=Folkeregister%20-%20Flytting%20og%20adresse%20-%20NY20&utm_term=adresseendring&utm_content=Adresseendring")}>
+                <View style = {styles.centerButtonsContainer}>
+                    <Button title="Endre adresse" color="#008b8b" onPress={() => Linking.openURL("https://www.skatteetaten.no/person/folkeregister/flytte/?utm_source=bing&utm_medium=cpc&utm_campaign=Folkeregister%20-%20Flytting%20og%20adresse%20-%20NY20&utm_term=adresseendring&utm_content=Adresseendring")}>
                     </Button>
-                    <Button title="Endre E-pot og mobilnummer" color="#66CDAA" onPress={() => numberPressButton()} />
                 </View>
             </View>
         </View>
@@ -61,22 +70,40 @@ export default function Profile() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        backgroundColor: "#87CEEB"
     },
-    buttonContainer: {
-        margin: 80,
-        height: 80,
-        width: 80,
+    upperScreen:{
+        height: '20%',
+        width: '100%',
+        alignItems: 'center',
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: '5%'
+
+
+    },
+    upperButtonContainer: {
+        margin: 10,
+        height: 50,
+        width: 50,
         borderRadius: 5,
-        padding: 20,
-        backgroundColor: "white"
+    },
+    centerButtonsContainer: {
+        width: '50%',
+        padding: 10,
+        flexDirection: 'column',
+        alignItems: "stretch",
+        justifyContent: "space-around",
+
+
     },
     userInfoContainer:{
         height: 50,
         width: 200,
         borderBottomColor: 'gray',
         borderBottomWidth: 1,
-        justifyContent: "space-between",
+        alignItems: 'center',
+
     },
     images: {
         width: "100%",
@@ -88,7 +115,7 @@ const styles = StyleSheet.create({
         height: 110,
         borderRadius: 5,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: "space-around",
     },
     textStyle: {
         fontSize: 15,
