ef6f2a5eab9ef07e3dfd3fbd00ebd3cbc1ea4eb8
Hollum <hollum@hotmail.com>
2020-07-06 13:06:42 +0200

    Login with MinID - successful redirect back to app with response code printed out

--- Components/Login/Login2.tsx
+++ Components/Login/Login2.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import {View, Text, Button, StyleSheet, Platform} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as AuthSession from "expo-auth-session";
 import {makeRedirectUri, useAuthRequest} from "expo-auth-session";
-import * as Linking from 'expo-linking'
+
 
 /*
 1. If the app is already open, the app is foregrounded and a Linking event is fired
@@ -13,8 +13,6 @@ You can handle these events with ------> Linking.addEventListener('url', callbac
 2. If the app is not already open, it is opened and the url is passed in as the initialURL
 You can handle these events with  -------> Linking.getInitialURL -- it returns a Promise that resolves to the url, if there is one.
  */
-
-
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -25,23 +23,10 @@ const discovery = {
     revocationEndpoint: 'https://oidc-ver1.difi.no/idporten-oidc-provider/revoke","jwks_uri":"https://oidc-ver1.difi.no/idporten-oidc-provider/jwk',
 };
 
-const useProxy = true;
 
-const redirectUri = AuthSession.makeRedirectUri({
-    native: 'digitalborger://redirect',
-    useProxy,
-});
 export default function Login({navigation}) {
-    //const discovery = AuthSession.useAutoDiscovery('https://oidc-ver1.difi.no/idporten-oidc-provider/.well-known/openid-configuration');
 
-    const discovery = {
-        authorizationEndpoint: 'https://oidc-ver1.difi.no/idporten-oidc-provider/authorize',
-        tokenEndpoint: 'https://oidc-ver1.difi.no/idporten-oidc-provider/token',
-        revocationEndpoint: 'https://oidc-ver1.difi.no/idporten-oidc-provider/revoke","jwks_uri":"https://oidc-ver1.difi.no/idporten-oidc-provider/jwk',
-    };
-
-    // Create and load an auth request
-    const [request, result, promptAsync] = AuthSession.useAuthRequest(
+    const [request, response, promptAsync] = useAuthRequest(
         {
             clientId: 'digdircamp_oidc',
             scopes: ["openid profile"],
@@ -49,15 +34,35 @@ export default function Login({navigation}) {
             usePKCE: true,
             state: "abcd", //randome
             codeChallenge: "1234", //randome
-            redirectUri,
+            // For usage in managed apps using the proxy
+            redirectUri: "digitalborger://redirect",
         },
         discovery
     );
 
+
+    React.useEffect(() => {
+        if (response?.type === 'success') {
+            const { code } = response.params;
+        }
+    }, [response]);
+
+    async function OpenWebPage() {
+        let result = await WebBrowser.openBrowserAsync("https://www.digdir.no/");
+    }
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Button title="Login!" disabled={!request} onPress={() => promptAsync({ useProxy })} />
-            {result && <Text>{JSON.stringify(result, null, 2)}</Text>}
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Login Screen</Text>
+            <Button title={"Home"} onPress={() => navigation.navigate("ScreenTabs")}/>
+            <Button title={"Open webbrowser"} onPress={OpenWebPage}/>
+            <Button
+                disabled={!request}
+                title="Login"
+                onPress={() => {
+                    promptAsync();
+                }}
+            />
         </View>
     );
 }
