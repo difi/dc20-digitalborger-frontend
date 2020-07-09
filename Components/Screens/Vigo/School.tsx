f71d55e0447a3cfb4a9e18f4451868c649c80214
Hege <hegeaal@stud.ntnu.no>
2020-07-09 09:16:09 +0200

    div changes

--- Components/Screens/Vigo/School.tsx
+++ Components/Screens/Vigo/School.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button, TouchableOpacity } from "react-native";
 import * as Location from "expo-location";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { FlatList } from "react-native-gesture-handler";
 
 const schools = [
   { name: "Sogndal Vgs", gpa: 4.5, url: "https://www.sogndal.vgs.no" },
@@ -13,19 +14,21 @@ const schools = [
 ];
 
 export default function School() {
-  return (
-    <View style={styles.container}>
-      <Icon name="office-building" size="40"></Icon>
-      <Text style={styles.name}>{"Voss Gymnas"}</Text>
-      <Text style={styles.gpa}>{"5.5"}</Text>
-      <TouchableOpacity style={styles.link}>
-        <Text>Gå til</Text>
-        <View style={styles.icon}>
-          <Icon name="arrow-right-circle-outline" size="20"></Icon>
-        </View>
-      </TouchableOpacity>
+  const [data, setData] = useState({ schools: [] });
+
+  useEffect( () => {
+    fetch("http://feat01-drupal8.dmz.local/dib/school/0/0/5", {
+      method: "GET"
+    })
+      .then(item => console.log(item))
+  })
+
+  return(
+    <View>
+      <Text>hei</Text>
     </View>
-  );
+  )
+  
 }
 
 const styles = StyleSheet.create({
@@ -45,9 +48,25 @@ const styles = StyleSheet.create({
   },
   link: {
     flexDirection: "row",
-    marginLeft: 90
+    marginLeft: 90,
   },
   icon: {
     marginLeft: 10,
   },
 });
+
+
+/*
+return (
+    <View style={styles.container}>
+      <Icon name="office-building" size="40"></Icon>
+      <Text style={styles.name}>{"Voss Gymnas"}</Text>
+      <Text style={styles.gpa}>{"5.5"}</Text>
+      <TouchableOpacity style={styles.link}>
+        <Text>Gå til</Text>
+        <View style={styles.icon}>
+          <Icon name="arrow-right-circle-outline" size="20"></Icon>
+        </View>
+      </TouchableOpacity>
+    </View>
+  ); */
