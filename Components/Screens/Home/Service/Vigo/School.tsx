21f6aeb2b32cd333f2b534c98ab2755547c5ae8c
Hege <hegeaal@stud.ntnu.no>
2020-07-13 12:59:45 +0200

    fixed fetch issues

--- Components/Screens/Home/Service/Vigo/School.tsx
+++ Components/Screens/Home/Service/Vigo/School.tsx
@@ -1,78 +1,72 @@
-import React, { useState, useLayoutEffect } from "react";
-import { Text, View, StyleSheet, TouchableOpacity, Linking } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
 import * as Location from "expo-location";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import axios from "axios";
 
-// PROBLEM: Rendrer ikke skolene før man lager koden :-(
-
 export default function School() {
-  const [data, setData] = useState(Array);
-  const [location, setLocation] = useState(null);
+  const [schools, setSchools] = useState(Array);
+  const [location, setLocation] = useState();
   const [errorMsg, setErrorMsg] = useState("");
-  let longitude = "";
-  let latitude = "";
+  const [status, setStatus] = useState("");
 
-  useLayoutEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        "http://feat01-drupal8.dmz.local/dib/school/" +
-          longitude +
-          "/" +
-          latitude +
-          "/5"
-      );
-      setData(result.data);
-    };
-    fetchData();
-    console.log(data);
-  }, []);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     (async () => {
       let { status } = await Location.requestPermissionsAsync();
+      setStatus(status);
       if (status !== "granted") {
         setErrorMsg("Permission to access location was denied");
       }
+      let location= await Location.getCurrentPositionAsync({});
+      setLocation(location); //trenger egentlig ikke lagre location
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+      let result = await axios(
+        "http://feat01-drupal8.dmz.local/dib/school/" +
+          location.coords.longitude +
+          "/" +
+          location.coords.latitude +
+          "/5"
+      );
+      setSchools(result.data);
     })();
   }, []);
 
-  let text = "Waiting..";
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-    longitude = location.coords.longitude;
-    latitude = location.coords.latitude;
+  if (status == "granted") {
+    return (
+      <View>
+        {schools.map((school, index) => (
+          <View key={index} style={styles.container}>
+            <Icon name="office-building" size={40}></Icon>
+            <Text style={styles.name}>{school.Skolenavn}</Text>
+            <Text style={styles.distance}>{school.Distanse + "km"}</Text>
+            <TouchableOpacity
+              style={styles.link}
+              onPress={() => Linking.openURL("http://" + school.Webside)}
+            >
+              <Text>Gå til</Text>
+              <View style={styles.icon}>
+                <Icon name="arrow-right-circle-outline" size={20}></Icon>
+              </View>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        <Text>{errorMsg}</Text>
+        <Text>For å se skoler i ditt nærområde, skru på stedsposisjon i instillinger.</Text>
+      </View>
+    );
   }
-
-  return (
-    <View>
-      {data.map((school, index) => (
-        <View key={index} style={styles.container}>
-          <Icon name="office-building" size="40"></Icon>
-          <Text style={styles.name}>{school.Skolenavn}</Text>
-          <Text style={styles.distance}>{school.Distanse + "km"}</Text>
-          <TouchableOpacity
-            style={styles.link}
-            onPress={() =>
-              Linking.openURL(
-                "http://" + school.Webside
-              )
-            }
-          >
-            <Text>Gå til</Text>
-            <View style={styles.icon}>
-              <Icon name="arrow-right-circle-outline" size="20"></Icon>
-            </View>
-          </TouchableOpacity>
-        </View>
-      ))}
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
