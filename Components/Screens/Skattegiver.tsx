63daf993ae3479d910b448dbe390b08a3297945f
ingvildvarahagen <ingvild.varahagen@gmail.com>
2020-07-14 08:17:25 +0200

    Skattegiver update

--- Components/Screens/Skattegiver.tsx
+++ Components/Screens/Skattegiver.tsx
@@ -7,12 +7,15 @@ import {TouchableOpacity} from "react-native-gesture-handler";
 import * as WebBrowser from "expo-web-browser";
 
 
-const text = {
+const info = {
     intro: "Disse har hentet skattekortet ditt:",
+    fetched: "Hentet:",
     domstol: "DOMSTOLENE I NORGE",
+    domstolDate: "12.04.2020",
     digdir: "DIGITALISERINGSDIREKTORATET",
+    digdirDate: "24.06.2020",
     sogndal: "SOGNDAL KOMMUNE",
-
+    sogndalDate: "18.08.2020",
 
 };
 
@@ -21,44 +24,55 @@ export default function Skattegiver() {
 
         <View>
             <View style={styles.container}>
-                <View style={styles.textContainer}>
-                    <Text style={styles.introText}>{text.intro}</Text>
+                <View style={styles.introText}>
+                    <Text style={{ fontWeight: "bold", fontSize: 15}}>{info.intro}</Text>
                 </View>
+                <View style={styles.textContainer}>
+                    <View style={styles.leftText}>
+                        <Text style={{fontSize: 15}}>{info.domstol}</Text>
+                    </View>
+                    <View style={styles.rightText}>
+                        <Text style={{fontSize: 15}}>{info.fetched}</Text>
+                        <Text style={{ fontWeight: "bold", fontSize: 15}}>{info.domstolDate}</Text>
+                    </View>
+                </View>
+                <View style={styles.textContainer}>
+                    <View style={styles.leftText}>
+                        <Text style={{fontSize: 15}}>{info.digdir}</Text>
+                    </View>
+                    <View style={styles.rightText}>
+                        <Text style={{fontSize: 15}}>{info.fetched}</Text>
+                        <Text style={{ fontWeight: "bold", fontSize: 15}}>{info.digdirDate}</Text>
+                    </View>
+                </View>
+                <View style={styles.textContainer}>
+                    <View style={styles.leftText}>
+                        <Text style={{fontSize: 15}}>{info.sogndal}</Text>
+                    </View>
+                    <View style={styles.rightText}>
+                        <Text style={{fontSize: 15}}>{info.fetched}</Text>
+                        <Text style={{ fontWeight: "bold",fontSize: 15}}>{info.sogndalDate}</Text>
+                    </View>
+                </View>
+
+
+
+                <TouchableOpacity
+                    containerStyle={styles.linkContainer}
+                    onPress={() =>
+                        WebBrowser.openBrowserAsync(
+                            "https://skatt.skatteetaten.no/web/minskatteside/?innvalg=minearbeidsgivere#/minearbeidsgivere"
+                        )
+                    }
+                >
+                    <View style={styles.linkContainer}>
+                        <Icon name="arrow-circle-right" size={15}></Icon>
+                        <Text style={{ fontWeight: "bold",fontSize: 13}}> Detaljer om hvem som har hentet skattekorte ditt </Text>
+                    </View>
+                </TouchableOpacity>
             </View>
 
-            <View style={styles.container}>
-                <View style={styles.textContainer}>
-                    <Text style={styles.textContainer}>{text.domstol}</Text>
-                </View>
             </View>
-
-            <View style={styles.container}>
-                <View style={styles.textContainer}>
-                    <Text style={styles.textContainer}>{text.digdir}</Text>
-                </View>
-            </View>
-
-            <View style={styles.container}>
-                <View style={styles.textContainer}>
-                    <Text style={styles.textContainer}>{text.sogndal}</Text>
-                </View>
-            </View>
-
-
-            <TouchableOpacity
-                containerStyle={styles.linkContainer}
-                onPress={() =>
-                    WebBrowser.openBrowserAsync(
-                        "https://skatt.skatteetaten.no/web/minskatteside/?innvalg=minearbeidsgivere#/minearbeidsgivere"
-                    )
-                }
-            >
-                <View style={styles.linkContainer}>
-                    <Icon name="arrow-circle-right" size={20}></Icon>
-                    <Text style={styles.linkText}> Detaljer om hvem som har {"\n"} hentet skattekorte ditt </Text>
-                </View>
-            </TouchableOpacity>
-        </View>
 
     );
 }
@@ -68,43 +82,38 @@ export default function Skattegiver() {
 const styles = StyleSheet.create({
 
     container: {
-        marginTop: 10,
-        marginBottom: 10,
-        top: 30,
-        borderBottomColor: "#E1E1E1",
-        borderBottomWidth: 1,
-        borderRadius: 30,
-
-    },
-
-    linkContainer:{
-        flex: 1,
-        flexDirection: "row",
-        alignItems: "center",
-        left: "4%",
         top: 10,
-
-    },
-
-    text:{
-        fontSize: 15,
-        margin: 10,
-        top: "5%",
-    },
-
-
-    textContainer:{
-    left: 10,
     },
 
     introText:{
-        fontWeight: "bold",
-        fontSize: 15,
-        marginLeft: 3,
+        borderBottomColor: "black",
+        borderBottomWidth: 1,
+        marginBottom: 10,
     },
 
-    linkText:{
-        fontSize: 16,
-        fontWeight: "bold",
+    textContainer:{
+        flexDirection: "row",
+        marginTop: 10,
+        borderBottomColor: "#E1E1E1",
+        borderBottomWidth: 1,
     },
+
+    rightText:{
+        alignItems: "flex-start",
+        marginBottom: 10,
+        right: 5,
+    },
+
+    leftText:{
+        alignItems: "flex-start",
+        flexDirection: "column",
+        marginBottom: 10,
+        flex: 1,
+    },
+
+    linkContainer:{
+        flexDirection: "row",
+        marginTop: 15,
+    },
+
 });
