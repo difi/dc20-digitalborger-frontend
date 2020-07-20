3177a817adfb8b5e6f978a4217505159dca98689
Hollum <hollum@hotmail.com>
2020-07-20 13:33:39 +0200

    spring function on login with layout implemented

--- Components/Login/Login.tsx
+++ Components/Login/Login.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
-import {View, Text, Button, StyleSheet, Platform, TouchableOpacity, Dimensions} from 'react-native';
+import {Image, SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import * as AuthSession from "expo-auth-session";
-import {makeRedirectUri, ResponseType, useAuthRequest} from "expo-auth-session";
-import * as Linking from 'expo-linking'
-import Axios from "axios";
+import {ResponseType, useAuthRequest} from "expo-auth-session";
 import {storeData} from "../Storage";
+import Animated, {Easing, Extrapolate, interpolate} from "react-native-reanimated";
+import {timing, useSpringTransition} from "react-native-redash";
+import {useEffect} from "react";
+import Button from "../Button";
+
+// @ts-ignore
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 let jwtDecode = require('jwt-decode');
 
 WebBrowser.maybeCompleteAuthSession();
@@ -20,6 +25,10 @@ const discovery = {
 };
  */
 
+const assets = [
+    require("../../assets/Illustrations/idporten.png"),
+]
+
 const useProxy = true;
 
 const redirectUri = AuthSession.makeRedirectUri({
@@ -27,7 +36,6 @@ const redirectUri = AuthSession.makeRedirectUri({
     useProxy,
 });
 
-console.log(redirectUri)
 export default function Login({navigation}) {
     //const discovery = AuthSession.useAutoDiscovery('https://oidc-ver1.difi.no/idporten-oidc-provider/.well-known/openid-configuration');
 
@@ -91,18 +99,86 @@ export default function Login({navigation}) {
                 .then(() => navigation.navigate("ScreenTabs"))
                 .catch(err => console.log(err))
 
-        } else {
-            alert("error!");
         }
     }, [result]);
 
+     const value = timing({
+         duration: 200,
+         from: 0,
+         to: 1,
+         easing: Easing.linear,
+     });
+
+     const transition = useSpringTransition(value);
+
+     const scale = interpolate(transition, {
+         inputRange: [0,1],
+         outputRange: [0,1]
+     });
+
+     const opacity = interpolate(value,
+         {
+             inputRange: [0,1],
+             outputRange: [0.5,1]
+         });
+
+
     return (
-       <View style={{flex: 1, paddingRight: 20, paddingLeft: 20}}>
-           <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-                <Text style={{fontWeight: "bold", fontSize: 20, textTransform: "uppercase"}}>
-                    Digital borger
-                </Text>
+       <View style={{flex: 1, backgroundColor: "white"}}>
+           <View style={{flex: 0.5, justifyContent: "center", alignItems: "center", marginTop: 40}}>
+               <Text style={{fontWeight: "bold", fontSize: 25, letterSpacing: 0.6}}>
+                   Velkommen til ID-porten
+               </Text>
+               <Text style={{fontStyle: "italic", fontSize: 20, marginTop: 10, color: "#666666", letterSpacing: 0.6}}>
+                   - Vi er til for din sikkerhet
+               </Text>
            </View>
+           <Animated.View style={{transform: [{scale}], flex: 1, justifyContent: "center"}}>
+               <Image source={assets[0]} style={{width: "75%", height: "75%", alignSelf: "center",}} resizeMode={"contain"}/>
+           </Animated.View>
+
+           <View style={{flex: 0.5, justifyContent: "center", alignItems: "center", marginRight: 40, marginLeft: 40}}>
+               <Text style={{letterSpacing: 0.6}}>
+                   Gjennom oss har du mulighet til å logge in på hvilken som helst offentlige tjeneste. Hvor kult er ikke det!
+               </Text>
+           </View>
+
+           <View style={{flex: 0.7, alignItems: "center", backgroundColor: "#5CB6B0", borderTopLeftRadius: 65, borderTopRightRadius: 65,
+               shadowColor: "#000",
+               shadowOffset: {
+                   width: 0,
+                   height: 2,
+               },
+               shadowOpacity: 0.3,
+               shadowRadius: 3,
+
+               }}>
+
+                   <View style={{flex: 1, marginTop: 40, marginBottom: 30}}>
+                       <Button onPress={() => promptAsync({ useProxy })}  variant={"default"} label={"Login"}/>
+                   </View>
+                   <View style={{flex: 2, flexDirection: "row"}}>
+                       <Text style={{color: "white", marginRight: 10, textDecorationLine: "underline", letterSpacing: 0.6}}>
+                            Opprett minID
+                       </Text>
+                   </View>
+               <TouchableOpacity onPress={() => navigation.goBack()} style={{alignSelf: "flex-start", marginLeft: 20, marginBottom: 20, flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+                   <FontAwesome name={"arrow-circle-left"} color={"white"} size={25} />
+                   <Text style={{color: "white", fontWeight: "bold", marginLeft: 10}}>
+                       Tilbake
+                   </Text>
+               </TouchableOpacity>
+
+           </View>
+       </View>
+    );
+}
+
+/*
+  <View style={{flex: 1, paddingRight: 20, paddingLeft: 20}}>
+           <Animated.View style={{opacity, flex: 1, justifyContent: "center", alignItems: "center", }}>
+               <Image source={assets[0]} style={{width: "75%", height: "75%", alignSelf: "center",}} resizeMode={"contain"}/>
+           </Animated.View>
            <View style={{ flex: 1, alignItems: 'center' }}>
                <TouchableOpacity disabled={!request} onPress={() => promptAsync({ useProxy })} style={{backgroundColor: "#6064E5", height: 50, width: "70%", borderRadius: 10, justifyContent: "center", alignItems: "center"}}>
                    <Text style={{fontWeight: "bold", fontSize: 20, textTransform: "uppercase", color: "white"}}>
@@ -113,10 +189,7 @@ export default function Login({navigation}) {
                {result && <Text>{JSON.stringify(result, null, 2)}</Text>}
            </View>
        </View>
-    );
-}
-
-
+ */
 
 
 
