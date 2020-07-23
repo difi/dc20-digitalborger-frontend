ae769e03b5951e81f4dad598f412460c1ecdd8e6
Hege <hegeaal@stud.ntnu.no>
2020-07-23 11:47:18 +0200

    new design on countdown

--- Components/ServerCommunications/Services/Politi.ts
+++ Components/ServerCommunications/Services/Politi.ts
@@ -1,21 +1,24 @@
-import {apiClient} from "../AxiosClient";
-import {AxiosError} from "axios";
+import { apiClient } from "../AxiosClient";
+import { AxiosError } from "axios";
 
-
-
-export const getTrafficStations = async (longitude: number, latitude: number, amount: number) => {
-    try{
-        const response = await apiClient.get("trafficstations/" + longitude + "/" + latitude + "/" + amount);
-        if (response.status !== 404){
-            return response.data;
-        }
-
-    }catch (e) {
-        if (e && e.response){
-            const axiosError = e as AxiosError;
-            return axiosError.response?.data;
-        }
-
-        throw e;
+export const getTrafficStations = async (
+  longitude: number,
+  latitude: number,
+  amount: number
+) => {
+  try {
+    const response = await apiClient.get(
+      "police/" + longitude + "/" + latitude + "/" + amount
+    );
+    if (response.status !== 404) {
+      return response.data;
     }
-}
+  } catch (e) {
+    if (e && e.response) {
+      const axiosError = e as AxiosError;
+      return axiosError.response?.data;
+    }
+
+    throw e;
+  }
+};
